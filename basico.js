320494baf131f013eaee56b4892d8e2d758f5373
mardenrafael <mardenrafaeldalmagro.gimenez@gmail.com>
2023-11-02 19:25:34 -0400

    criado basico.js

---
+++ basico.js
@@ -0,0 +1,89 @@
+/**
+ *
+ * Variaveis
+ *
+ */
+
+let minhaVariavel = "";
+const minhaConstante = "ola";
+
+/**
+ *
+ * Tipo de dados
+ *
+ */
+
+(""); // String
+10; // Number
+[10]; // Array
+let celular = {
+  processador: "i3",
+}; // Objeto
+
+/**
+ *
+ * Funções
+ *
+ *
+ */
+
+// let meuNumeroUm = 1;
+
+// function somaMaisDois(numero) {
+//   console.log(numero + 2);
+
+//   return "xuxa";
+// }
+
+// let minhaXuxa = somaMaisDois(10);
+
+// console.log(minhaXuxa);
+
+/**
+ *
+ * Estruturas condicionais
+ *
+ */
+
+// let nome = "Naiely";
+
+// if (nome == "Marden") {
+//   console.log("Marden verdade");
+// } else if (nome == "Naiely") {
+//   console.log("Nai verdade");
+// } else {
+//   console.log("Não achei ngm");
+// }
+
+// switch (nome) {
+//   case "Marden":
+//     console.log("Marden verdade");
+//     break;
+
+//   case "Naiely":
+//     console.log("Nai verdade");
+//     break;
+
+//   default:
+//     console.log("Não achei ngm");
+//     break;
+// }
+
+/**
+ *
+ * Estrutura de repetição
+ */
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+let j = 0;
+while (j < 100) {
+  console.log("verdade");
+  j++;
+}
+
+do {
+  console.log("verdade dnv");
+} while (false);
